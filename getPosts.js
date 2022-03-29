a518501e8cd3f46cf30b37b7d0a864a6d2f316b4
Florentinus Aan Setiawan <79754762+florentinus28@users.noreply.github.com>
2022-03-29 20:01:54 +0700

    Update getPosts.js

--- getPosts.js
+++ getPosts.js
@@ -14,7 +14,7 @@ export default function main(){
                 },
             }
         );
-        console.log(response.body)
+//         console.log(response.body)
         check(response, {
             'Get Employee List is status 200': (r) => r.status === 200
         });
